80f60ee7e57a86e60079bb3e5f2ba5543e57cc03
unknown <fboy4069@gmail.com>
2022-09-25 19:54:02 +0600

    width fixed

--- src/FirestoreOperation/FirestoreOperation.js
+++ src/FirestoreOperation/FirestoreOperation.js
@@ -68,7 +68,7 @@ const FireStoreOperation = () => {
             <div className=' flex justify-center mt-3'>
                 <div className='w-1/4'>
                     <form className='bg-slate-200 py-2 px-4 rounded-lg flex justify-between' onSubmit={handleSubmit}>
-                        <input className='p-2 border rounded-lg w-full border-teal-900' type="text" ref={addPlayers} />
+                        <input className='p-2 border rounded-lg min-w-full max-w-full w-screen  border-teal-900' type="text" ref={addPlayers} />
                         <input className='py-2 px-4 bg-teal-900 text-white rounded-lg hover:bg-teal-700 transition-all cursor-pointer ml-3' type="submit" value="Add" />
                     </form>
                 </div>
